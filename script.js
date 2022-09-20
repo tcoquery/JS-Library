9d3f05e14efbcb3df66b81326aec21f2b387667e
tcoquery <tcoquery@gmail.com>
2022-09-20 15:33:11 +0200

    refactoring

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function Book(title, author, pages, read) {
     this.author = author
     this.pages = pages
     this.read = read
-    }
+}
 
 function addBookToLibrary(title, author, pages, read) {
     let newBook = new Book(title, author, pages, read);
@@ -35,6 +35,41 @@ function firstRow() {
     row.appendChild(pages);
     row.appendChild(read);    
     row.appendChild(empty); 
+}
+
+function bookRows() {
+    for (let i = 0; i < myLibrary.length ; i++) {
+        const newRow = document.createElement("tr");
+        const newTitle = document.createElement("td");
+        const newAuthor = document.createElement("td");
+        const newPages = document.createElement("td");        
+        const newRead = document.createElement("td");
+        const newRemove = document.createElement("td");
+        const readButton = document.createElement("button");
+        const removeButton = document.createElement("img");
+        newTitle.textContent = myLibrary[i].title;
+        newAuthor.textContent = myLibrary[i].author;
+        newPages.textContent = myLibrary[i].pages;
+        readButton.textContent = myLibrary[i].read;
+        removeButton.textContent = "Delete";
+        removeButton.classList.add("remove")
+        if(myLibrary[i].read == "yes") {
+            readButton.classList.add("read")
+        } else { 
+            readButton.classList.add("unread")
+        }
+        removeButton.dataset.indexNumber = i;
+        removeButton.src = "trash.png"
+        readButton.dataset.indexNumber = i;
+        library.appendChild(newRow)
+        newRow.appendChild(newTitle);
+        newRow.appendChild(newAuthor);
+        newRow.appendChild(newPages);
+        newRow.appendChild(newRead);
+        newRow.appendChild(newRemove);
+        newRead.appendChild(readButton);
+        newRemove.appendChild(removeButton);
+    }
 }
 
 function deleteButtons() {
@@ -70,64 +105,32 @@ function readButtons() {
 
 function showBooks() {  
     library.textContent = "";
-    if (myLibrary.length === 0) {
-        return
-    } else {
+    if (myLibrary.length === 0) return
+    else {
         firstRow();
-        for (let i = 0; i < myLibrary.length ; i++) {
-            const newRow = document.createElement("tr");
-            const newTitle = document.createElement("td");
-            const newAuthor = document.createElement("td");
-            const newPages = document.createElement("td");        
-            const newRead = document.createElement("td");
-            const newRemove = document.createElement("td");
-            const readButton = document.createElement("button");
-            const removeButton = document.createElement("img");
-            newTitle.textContent = myLibrary[i].title;
-            newAuthor.textContent = myLibrary[i].author;
-            newPages.textContent = myLibrary[i].pages;
-            readButton.textContent = myLibrary[i].read;
-            removeButton.textContent = "Delete";
-            removeButton.classList.add("remove")
-            if(myLibrary[i].read == "yes") {
-                readButton.classList.add("read")
-            } else { 
-                readButton.classList.add("unread")
-            }
-            removeButton.dataset.indexNumber = i;
-            removeButton.src = "trash.png"
-            readButton.dataset.indexNumber = i;
-            library.appendChild(newRow)
-            newRow.appendChild(newTitle);
-            newRow.appendChild(newAuthor);
-            newRow.appendChild(newPages);
-            newRow.appendChild(newRead);
-            newRow.appendChild(newRemove);
-            newRead.appendChild(readButton);
-            newRemove.appendChild(removeButton);
-        }
+        bookRows();
         deleteButtons();
         readButtons();
-        }
     }
+}
     
 
-    btn.addEventListener("click", () => {
-        const t = document.getElementById('title').value 
-        const a = document.getElementById('author').value 
-        const p = document.getElementById('pages').value 
-        let r = document.getElementById('status').checked 
-        if(t === "" || a === "" || p === "") {
-            alert("You must fill all fields !")
-        }
-        else if(r == true) {
-            r = "yes";
-            addBookToLibrary(t,a,p,r);
-        } else {
-            r = "no";
-            addBookToLibrary(t,a,p,r);
-        }
-        showBooks();
-    });
+btn.addEventListener("click", () => {
+    const t = document.getElementById('title').value 
+    const a = document.getElementById('author').value 
+    const p = document.getElementById('pages').value 
+    let r = document.getElementById('status').checked 
+    if(t === "" || a === "" || p === "") {
+        alert("You must fill all fields !")
+    }
+    else if(r == true) {
+        r = "yes";
+        addBookToLibrary(t,a,p,r);
+    } else {
+        r = "no";
+        addBookToLibrary(t,a,p,r);
+    }
+    showBooks();
+});
 
 
